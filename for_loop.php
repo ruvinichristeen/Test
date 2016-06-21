3b8fe168eae611196391263333e4916ac500ceb5
ruvinichristeen <ruviniwijenayaka@gmail.com>
2016-06-21 12:22:10 +0530

    initial commit

---
+++ for_loop.php
@@ -0,0 +1,17 @@
+<?php 
+//for loop
+for($i=0; $i<10; $i++){
+	echo "The number is: $i <br>";
+}
+echo"<br>";
+echo"<br>";
+?>
+
+<?php
+//foreach loop
+$vehicles=array("car","van","bus","threewheel");
+foreach ($vehicles as $x){
+	echo "$x<br>";
+} 
+
+?>
